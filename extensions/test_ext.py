76cde6b2280f1da5a31bfb53f2415de1d6fc79d0
Elo Lewis <elojlewis@gmail.com>
2025-03-20 21:09:15 -0400

    Add configuration for cache file and create custom page in Streamlit app

---
+++ extensions/test_ext.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    st.title("My Custom Page")
+    st.write("This page was dynamically added to the app!")
+    st.write("You can add widgets, charts, and more here.")
